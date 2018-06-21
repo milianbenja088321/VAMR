77a738bb8e2102ec2a27aa1e3370eac0f1b30648
milianbenja088321 <vinyelortiz@gmail.com>
2018-06-20 22:14:19 -0400

    Moved Folder
    Bulk folder added

---
+++ VRAR/Assets/Scripts/CameraDetect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDetect : MonoBehaviour
+{
+
+    string camName = "THETA UVC FullHD Blender";
+    int cameraIndex;
+    [SerializeField] Material test = null;
+ 
+    WebCamDevice[] devices;
+    WebCamTexture cam;
+
+    // Use this for initialization
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        Debug.Log("Number of web cams connected: " + devices.Length);
+        Renderer rend = this.GetComponentInChildren<Renderer>();
+
+        WebCamTexture mycam = new WebCamTexture();
+        string camName = devices[2].name;
+        Debug.Log("The webcam name is " + camName);
+        mycam.deviceName = camName;
+        rend.material.mainTexture = mycam;
+        mycam.Play();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)) { }
+    }
+
+    void PlayCamera()
+    {
+        cam.Play();
+    }
+
+    void StopCamera()
+    {
+        cam.Stop();
+    }
+}
---
+++ VRAR/Assets/Scripts/Health.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour
+{
+    private const float maxHealth = 100.0f;
+    private bool isAlive { get; set; }
+
+    [SerializeField] private GameObject self;
+
+    // currentHealth linear change in network
+    // needs to be synced on Awake()
+    [SyncVar(hook = "OnChangeHealth")]
+    [SerializeField] private int currentHealth = (int)maxHealth;
+
+    private void Start()
+    {
+        isAlive = true;
+    }
+
+    public void TakeDamage(int _amount)
+    {
+        if (isServer == false)
+            return;
+
+        currentHealth -= _amount;
+
+        if(currentHealth <= 0)
+        {
+            // player is dead do we need to respawn?
+            RpcSetActive();
+        }
+    }
+
+    private void OnChangeHealth(int _health)
+    {
+        // this will get called everytime the currentHealth
+        // variable is changed
+        Debug.Log("Player took a hit of " + _health);
+        
+    }
+
+    [Command]
+    void CmdSetActive()
+    {
+        RpcSetActive();
+    }
+
+    [ClientRpc]
+    private void RpcSetActive()
+    {
+        self.SetActive(false);
+    }
+}
